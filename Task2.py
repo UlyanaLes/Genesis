9229f46a07602be27883401d33eb80de338c5d80
Ulyana_Liaskevich <ulles91@gmail.com>
2021-06-01 00:08:58 +0300

    test_new3

--- Task2.py
+++ Task2.py
@@ -3,3 +3,6 @@
 # Write a decorator to cache function invocation results.
 # Store pairs arg:result in a dictionary in an attribute of the function object. The function being memoized is fibonacci or factorial.
 # If you choose factorial please write your own function. Do not use module math.
+
+
+print('yeeeh')
